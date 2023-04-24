76b4ad2b08b59e2256eaad52b292b23fb8a91ff8
NDsasuke~ <nishaldranasinghe@gmail.com~>
2023-04-24 23:28:47 +0530

    sign up and login ui created

---
+++ website/views.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+import sqlite3
+from werkzeug.exceptions import abort
+
+views = Blueprint("views",__name__)
+
+@views.route("/home")
+@views.route("/")
+def home():
+    return render_template("home.html")
+
+@views.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    
+    return render_template('create.html')
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
